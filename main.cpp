d2d4c2aae1ab1c2ce0d9ca6c2b15091246bec2d3
yumcyawiz <sergeyinverse@gmail.com>
2018-09-29 19:18:50 +0900

    色々修正

--- main.cpp
+++ main.cpp
@@ -18,11 +18,11 @@ const double R_atmos = 6420 * 1000;
 
 
 const Vec3 sunDir = normalize(Vec3(0, -1, 0));
-const RGB sunColor = RGB(100);
+const RGB sunColor = RGB(50);
 
 
 const double rayleigh_scaleheight = 8000;
-const double mie_scaleheight = 2400;
+const double mie_scaleheight = 1200;
 const RGB beta_rayleigh = RGB(3.8e-6, 13.5e-6, 33.1e-6);
 const RGB beta_mie = RGB(21e-6);
 
@@ -37,16 +37,16 @@ RGB rayleigh_beta(double height) {
 
 RGB Tr(const Vec3& p1, const Vec3& p2) {
   Ray lightRay(p1, normalize(p2 - p1));
-  double rayleigh_optical_depth_light = 0;
-  double mie_optical_depth_light = 0;
+  double rayleigh_optical_depth = 0;
+  double mie_optical_depth = 0;
   double ds_light = (p2 - p1).length()/volume_samples;
   for(int j = 0; j < volume_samples; j++) {
     Vec3 p_light = lightRay(ds_light * (j + 1));
     double h_light = p_light.length() - R;
-    rayleigh_optical_depth_light += std::exp(-h_light/rayleigh_scaleheight) * ds_light;
-    mie_optical_depth_light += std::exp(-h_light/mie_scaleheight) * ds_light;
+    rayleigh_optical_depth += std::exp(-h_light/rayleigh_scaleheight) * ds_light;
+    mie_optical_depth += std::exp(-h_light/mie_scaleheight) * ds_light;
   }
-  return exp(-(beta_rayleigh * rayleigh_optical_depth_light + beta_mie * mie_optical_depth_light));
+  return exp(-(beta_rayleigh * rayleigh_optical_depth + beta_mie * mie_optical_depth));
 }
 
 
@@ -91,15 +91,17 @@ RGB Li(const Ray& _ray, const Scene& scene, Sampler& sampler) {
           beta *= 4*M_PI * Tr(p_prev, p);
           Vec3 wi = normalize(p - p_prev);
           rayleigh_coeff *= rayleigh_phase_function(wo, wi);
-          mie_coeff *= mie_phase_function(wo, normalize(p - p_prev), 0.76);
+          mie_coeff *= mie_phase_function(wo, wi, 0.76);
           wo = -wi;
+        }
+        else {
+          RGB tr = Tr(ray.origin, p);
+          beta *= tr;
         }
         double h = p.length() - R;
         if(h < 0) break;
         double h_rayleigh = std::exp(-h/rayleigh_scaleheight) * ds;
         double h_mie = std::exp(-h/mie_scaleheight) * ds;
-        RGB tr = Tr(ray.origin, p);
-        beta *= tr;
         
         //Direct Light
         Ray lightRay = Ray(p, sunDir);
@@ -115,6 +117,7 @@ RGB Li(const Ray& _ray, const Scene& scene, Sampler& sampler) {
     if(res.hitShape->type == "ground") {
       auto hitMaterial = res.hitShape->material;
       double cos = std::max(dot(res.hitNormal, sunDir), 0.0);
+      cos = 0;
       Ray lightRay = Ray(res.hitPos, sunDir);
       Hit light_res;
       scene.intersect(lightRay, light_res);
